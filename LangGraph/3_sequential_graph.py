2aef95ea2dbdf5096a8d4c88f0b2d58d9d1d1b5f
amolbarkale <barkaleamol@gmail.com>
2025-08-20 18:32:13 +0530

    update sequential graph

--- LangGraph/3_sequential_graph.py
+++ LangGraph/3_sequential_graph.py
@@ -5,15 +5,15 @@ from IPython.display import Image, display
 class AgnetState(TypedDict):
     name: str
     age: str
+    skills: list[str]
     final: str
-
 
 def first_node(state: AgnetState)->AgnetState:
     """
     This is the first nore of our sequence
     """
 
-    state["final"] = f"Hi {state['name']} !"
+    state["final"] = f"Hi {state['name']} welcome to the system."
 
     return state
 
@@ -22,21 +22,30 @@ def second_node(state: AgnetState)-> AgnetState:
     This is the second nore of our sequence
     """
 
-    state['final'] = state["final"] + f"You are {state['age']} years old !"
+    state['final'] = state["final"] + f" You are {state['age']} years old !"
 
     return state
+
+def third_node(state: AgnetState) -> AgnetState:
+    """
+    This is the third node of our sequence
+    """
+    state["final"] = state["final"] + f", Your skills are {", ".join(state['skills'])}"
 
 graph = StateGraph(AgnetState)
 
 graph.add_node("first_node", first_node)
 graph.add_node("second_node", second_node)
+graph.add_node("third_node", third_node)
 
 graph.set_entry_point("first_node")
 graph.add_edge("first_node", "second_node")
-graph.set_finish_point("second_node")
+graph.add_edge("second_node", "third_node")
+graph.set_finish_point("third_node")
 
 app = graph.compile()
 
 display(Image(app.get_graph().draw_mermaid_png))
 
-result = app.invoke({"name": "Charlie", "age": 20})
+result = app.invoke({"name": "Charlie", "age": 20, "skills": ["frontend", "backend", "AI", "ML"]})
+print('result:', result)
